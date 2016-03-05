fce6b5cbb83bb54ec29440482ab304ff6d2ee636
cchandurkar <cchandurkar@gmail.com>
2016-03-05 18:27:18 -0500

    gh-pages auto-update on push to master

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -2,17 +2,11 @@
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
-# only proceed script when started not by pull request (PR)
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-  echo "this is PR, exiting"
-  exit 0
-fi
-
 # build (CHANGE THIS)
 npm run build
 
 # Exit if branch is not master
-# if ["$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+if ["$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
 
 # config
 git config --global user.email "cchandurkar@gmail.com"
@@ -20,10 +14,6 @@ git config --global user.name "cchandurkar"
 
 #using token clone gh-pages branch
 git clone --quiet --branch=gh-pages "https://${GITHUB_TOKEN}@${GITHUB_REF}.git"  gh-pages > /dev/null
-
-#cd into gh-pages
-echo "Listing: ";
-ls
 
 # Copy Files
 cp -r ./docs ./gh-pages
@@ -35,7 +25,6 @@ cd gh-pages
 # deploy
 git add --all
 git commit -m "Travis Build $TRAVIS_BUILD_NUMBER for gh-pages"
-echo "Pushing https://${GITHUB_TOKEN}@${GITHUB_REF}.git";
 
 git push -fq origin gh-pages > /dev/null 2>&1
 

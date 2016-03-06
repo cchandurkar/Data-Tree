42ae23ee7b6fa058a65a52e53d5e4ff723a1cea1
cchandurkar <cchandurkar@gmail.com>
2016-03-05 19:08:37 -0500

    updated deploy script

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -6,27 +6,30 @@ set -o errexit
 npm run build
 
 # Exit if branch is not master
-if ["$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+if [[ "$TRAVIS_BRANCH" == "master" ]]
 
-# config
-git config --global user.email "cchandurkar@gmail.com"
-git config --global user.name "cchandurkar"
+  # config
+  git config --global user.email "cchandurkar@gmail.com"
+  git config --global user.name "cchandurkar"
 
-#using token clone gh-pages branch
-git clone --quiet --branch=gh-pages "https://${GITHUB_TOKEN}@${GITHUB_REF}.git"  gh-pages > /dev/null
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages "https://${GITHUB_TOKEN}@${GITHUB_REF}.git"  gh-pages > /dev/null
 
-# Copy Files
-cp -r ./docs ./gh-pages
-cp -a ./dist/. ./gh-pages
+  # Copy Files
+  cp -r ./docs ./gh-pages
+  cp -a ./dist/. ./gh-pages
 
-# Init
-cd gh-pages
+  # cd into gh-pages
+  cd gh-pages
 
-# deploy
-git add --all
-git commit -m "Travis Build $TRAVIS_BUILD_NUMBER for gh-pages"
+  # deploy
+  git add --all
+  git commit -m "Travis Build $TRAVIS_BUILD_NUMBER for gh-pages"
+  git push -fq origin gh-pages > /dev/null 2>&1
 
-git push -fq origin gh-pages > /dev/null 2>&1
+  # Remove tempGHPages directory
+  rm -rf gh-pages
 
-# Remove tempGHPages directory
-rm -rf gh-pages
+else
+  echo "gh-pages will be updated upon push or pull to master";
+fi
